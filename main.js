37b834ca634ea42404a159d8808a80c9f57f46b6
Raúl Pérez <jrpcassone@gmail.com>
2023-07-05 09:10:11 +0200

    Update main.js with main functions

---
+++ main.js
@@ -0,0 +1,30 @@
+const createCardsDeck = () => {
+  const suits = ["Corazones", "Bastos", "Espadas" ,"Diamantes"];
+  const ranks = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+  let newDeck = [];
+
+  suits.forEach(suit => {
+    ranks.forEach(rank => {
+      newDeck.push(`${rank} de ${suit}`) 
+  });
+});
+
+  return newDeck
+};
+
+
+const getRandomCard = (gameDeck) => {
+
+  const randomCard = gameDeck[Math.floor(Math.random() * gameDeck.length)];
+
+  return randomCard
+};
+
+
+const playGame = () => {
+  const deckToUse = createCardsDeck();
+  const randomCard = getRandomCard(deckToUse);
+
+
+  return randomCard
+};
